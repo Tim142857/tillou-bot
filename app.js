2982a2cf155823bd713acbdd50355eba3788c95d
Tim142857 <timothyleroch@gmail.com>
2024-01-22 14:48:46 +0100

    wip

--- app.js
+++ app.js
@@ -1,54 +1,63 @@
-const Discord = require('discord.js');
-const axios = require('axios');
-
-const client = new Discord.Client();
-const token = 'MTE5ODkxNTE4MDU1NDM3NTMyOQ.GYAENL.Ag4LdBfq1x_1N6GvPHgxuFpcJZj_m4kfv6LNUg';
-const channelId = '266259012633362454';
-
-client.on('ready', () => {
-    console.log(`Logged in as ${client.user.tag}`);
+require("dotenv").config(); // Charge les variables d'environnement depuis le fichier .env
+const token = process.env.DISCORD_BOT_TOKEN;
+const channelId = process.env.DISCORD_CHANNEL_ID;
+console.log("token: ", token);
+console.log("channelId: ", channelId);
+const { Client, GatewayIntentBits } = require("discord.js");
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+client.on("ready", () => {
+  console.log(`Logged in as ${client.user.tag}`);
 });
 
-client.on('message', async (message) => {
-    console.log('Message:', message.content);
-  
-    // Vérifiez si la commande est !checkPage
-    if (message.content.toLowerCase() === '!checkpage') {
-        try {
-            const url = 'https://openloot.com/marketplace?gameId=56a149cf-f146-487a-8a1c-58dc9ff3a15c&tag=NFT.Workshop.TimeWarden';
+client.on("message", async (message) => {
+  console.log("Message:", message.content);
 
-            // Effectuez une requête HTTP pour récupérer le contenu de la page
-            const response = await axios.get(url);
+  // Vérifiez si la commande est !checkPage
+  if (message.content.toLowerCase() === "!checkpage") {
+    try {
+      const url =
+        "https://openloot.com/marketplace?gameId=56a149cf-f146-487a-8a1c-58dc9ff3a15c&tag=NFT.Workshop.TimeWarden";
 
-            // Comptez le nombre de fois que "TimeWarden" apparaît dans le contenu de la page
-            const count = (response.data.match(/TimeWarden/gi) || []).length;
+      // Effectuez une requête HTTP pour récupérer le contenu de la page
+      const response = await axios.get(url);
 
-            // Envoyez le résultat sur le serveur Discord
-            const resultMessage = `Le nombre de fois que 'TimeWarden' est affiché sur la page est : ${count}`;
-            const channel = client.channels.cache.get(channelId);
+      // Comptez le nombre de fois que "TimeWarden" apparaît dans le contenu de la page
+      const count = (response.data.match(/TimeWarden/gi) || []).length;
 
-            if (channel) {
-                channel.send(resultMessage);
-                console.log('Résultat envoyé avec succès.');
-            } else {
-                console.error(`Le canal avec l'ID ${channelId} n'a pas été trouvé.`);
-            }
-        } catch (error) {
-            console.error('Une erreur s\'est produite lors de la récupération de la page :', error.message);
-            message.channel.send('Une erreur s\'est produite lors de la récupération de la page.');
-        }
+      // Envoyez le résultat sur le serveur Discord
+      const resultMessage = `Le nombre de fois que 'TimeWarden' est affiché sur la page est : ${count}`;
+      const channel = client.channels.cache.get(channelId);
+
+      if (channel) {
+        channel.send(resultMessage);
+        console.log("Résultat envoyé avec succès.");
+      } else {
+        console.error(`Le canal avec l'ID ${channelId} n'a pas été trouvé.`);
+      }
+    } catch (error) {
+      console.error(
+        "Une erreur s'est produite lors de la récupération de la page :",
+        error.message
+      );
+      message.channel.send(
+        "Une erreur s'est produite lors de la récupération de la page."
+      );
     }
+  }
 });
 
-client.on('error', (error) => {
-    console.error('Une erreur non traitée s\'est produite :', error);
+client.on("error", (error) => {
+  console.error("Une erreur non traitée s'est produite :", error);
 });
 
-client.login(token)
+client
+  .login(token)
   .then(() => {
-    console.log('Bot connecté avec succès !');
+    console.log("Bot connecté avec succès !");
   })
   .catch((error) => {
-    console.error('Une erreur s\'est produite lors de la connexion du bot :', error.message);
+    console.error(
+      "Une erreur s'est produite lors de la connexion du bot :",
+      error.message
+    );
   });
-
